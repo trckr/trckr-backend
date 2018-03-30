8b57a89bd031ea79d84a4c66dee659abcc1b1deb
Daniel Milenkovic <flipbug360@gmail.com>
2018-03-30 16:47:50 +0200

    add token-authentication for rest api

--- django/trckr_backend/urls.py
+++ django/trckr_backend/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/ping/', include('ping.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/auth/', include('rest_framework.urls')),
+    path('api/token-auth/', obtain_jwt_token),
 ]
