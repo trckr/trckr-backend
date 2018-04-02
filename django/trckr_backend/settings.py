1224ff2d5470609dbb3db4863bec69df5e5ef000
Daniel Milenkovic <flipbug360@gmail.com>
2018-04-02 20:23:41 +0200

    Increase token lifetime to a year

--- django/trckr_backend/settings.py
+++ django/trckr_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,6 +138,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication'
     ]
 
+}
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=365)
 }
 
 # It might be useful to consider CORS_ORIGIN_WHITELIST for the future
