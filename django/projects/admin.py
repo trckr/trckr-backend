ab5a377f3ea2d6f440cd72dbff506088702652b9
Daniel Milenkovic <flipbug360@gmail.com>
2018-04-04 18:49:39 +0200

    Set display field for project admin view

--- django/projects/admin.py
+++ django/projects/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Project
 
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Project, ProjectAdmin)
