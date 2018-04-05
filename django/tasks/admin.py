109e2faa4c7c8b0f50a645370feeea05f4deb7ab
Daniel Milenkovic <flipbug360@gmail.com>
2018-04-05 22:18:38 +0200

    Register TaskAdmin

--- django/tasks/admin.py
+++ django/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Task, TaskAdmin)
