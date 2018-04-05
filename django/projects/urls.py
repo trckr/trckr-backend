fa69cec4edb061750636d45745e3921bf1ce2e7c
Luca Christen <lucachristen94@gmail.com>
2018-04-05 17:13:25 +0200

    Extend REST API to query tasks for a specific project
    User can now query the database via a REST call like the
    following:
    
    /api/project/<projectId>/tasks
    
    Unit Tests for this part of the API are still pending.

--- django/projects/urls.py
+++ django/projects/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProjectView.as_view())
+    path('', views.ProjectView.as_view()),
+    path(r'<pk>/tasks', views.TaskProjectView.as_view())
 ]
--- django/projects/views.py
+++ django/projects/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
 
 from .models import Project
 from .serializers import ProjectSerializer
@@ -8,3 +12,19 @@ from .serializers import ProjectSerializer
 class ProjectView(ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+class TaskProjectView(APIView):
+    """
+    Let's users query tasks for a specific project
+    """
+    def get_object(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        project = self.get_object(pk)
+        tasks = Task.objects.filter(project=project)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
