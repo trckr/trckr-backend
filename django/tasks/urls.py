372df164ebe18acc76fb05f82a30450e84807225
Luca Christen <lucachristen94@gmail.com>
2018-04-16 20:58:17 +0200

    Extend REST API to query time entries for a specific task
    User can now query the database via a REST call like:
    
    /api/tasks/<taskId>/time-entries

--- django/tasks/urls.py
+++ django/tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TaskView.as_view(), name='tasks'),
-    path(r'<pk>', views.TaskDetailView.as_view(), name='tasks-detail')
+    path(r'<pk>', views.TaskDetailView.as_view(), name='tasks-detail'),
+    path(r'<pk>/time-entries', views.TaskTimeEntryView.as_view(), name='tasks-time-entries')
 ]
--- django/tasks/views.py
+++ django/tasks/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from time_entries.models import TimeEntry
+from time_entries.serializers import TimeEntrySerializer
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -47,3 +49,20 @@ class TaskDetailView(APIView):
             return Response(serializer.data)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskTimeEntryView(APIView):
+    """
+    Let's users query time entries for a specific task
+    """
+
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        task = self.get_object(pk)
+        time_entries = TimeEntry.objects.filter(task=task)
+        serializer = TimeEntrySerializer(time_entries, many=True)
+        return Response(serializer.data)
