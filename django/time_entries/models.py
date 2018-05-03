16664fad1d6848e2c1c47abb05838858ad1993e9
David Pacassi Torrico <dpacassi@users.noreply.github.com>
2018-05-03 22:53:09 +0200

    Add the project id to time entries

--- django/time_entries/models.py
+++ django/time_entries/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import  MinValueValidator
 from tasks.models import Task
+from projects.models import Project
 
 class TimeEntry(models.Model):
     description = models.TextField(blank=True, default="")
@@ -13,6 +14,7 @@ class TimeEntry(models.Model):
             )
 
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     createdBy = models.ForeignKey(User, on_delete=models.PROTECT)
     createdDate = models.DateTimeField(auto_now_add=True)
     modifiedDate = models.DateTimeField(auto_now=True)
