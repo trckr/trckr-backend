da5484812e2076299a074160d209b052d2223c15
Daniel Milenkovic <flipbug360@gmail.com>
2018-05-12 14:02:24 +0200

    Set startTime by default to now for time entries

--- django/time_entries/models.py
+++ django/time_entries/models.py
@@ -1,17 +1,18 @@
-from decimal import *
+from datetime import datetime
+from decimal import Decimal
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import  MinValueValidator
+from django.core.validators import MinValueValidator
 from tasks.models import Task
+
 
 class TimeEntry(models.Model):
     description = models.TextField(blank=True, default="")
-    startTime = models.DateTimeField()
+    startTime = models.DateTimeField(default=datetime.now, blank=True)
     timeSpent = models.DecimalField(
-            max_digits=10,
-            decimal_places=5,
-            validators=[MinValueValidator(Decimal('0.01'))]
-            )
+        max_digits=10,
+        decimal_places=5,
+        validators=[MinValueValidator(Decimal('0.01'))])
 
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     createdBy = models.ForeignKey(User, on_delete=models.PROTECT)
